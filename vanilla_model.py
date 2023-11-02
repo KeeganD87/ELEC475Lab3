baa3fe3868a5253c396ea8d8734cb9aa7d6316f2
Keegan Dillon <keeganegdillon@gmail.com>
2023-11-02 13:42:29 -0400

    imported model code from previous lab

--- vanilla_model.py
+++ vanilla_model.py
@@ -1,42 +1,74 @@
-import torch
+#   Partial implementation of:
+#       Huang & Belongie, "Arbitrary Style Transfer in Real-Time with Adaptive Instance Normalization", arXiv:1703.06868v2, 30 July 2017
+#
+#   Adapted from:
+#       https://github.com/naoto0804/pytorch-AdaIN
+#
+
 import torch.nn as nn
-import torchvision.models as models
 
-#Load the VGG model architecture (you can choose VGG16, VGG19, etc.)
-vgg_model = models.vgg16(pretrained=True)
-
-#Load the saved weights and state dictionary from the .pth file
-saved_model_state = torch.load('encoder.pth')
-
-#Load the saved weights into the model
-vgg_model.load_state_dict(saved_model_state)
-
-#Ensure the model is in evaluation mode if you're using it for feature extraction
-vgg_model.eval()
-
-class CustomFrontEnd(nn.Module):
-    def __init__(self, input_size, num_classes):
-        super(CustomFrontEnd, self).__init__()
-        self.fc1 = nn.Linear(input_size, 512)
-        self.relu1 = nn.ReLU()
-        self.fc2 = nn.Linear(512, num_classes)
-        self.softmax = nn.Softmax(dim=1)
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.relu1(x)
-        x = self.fc2(x)
-        x= self.softmax(x)
-        return x
-
-class VGGCustomModel(nn.Module):
-    def __init__(self, backend, frontend):
-        super(VGGCustomModel, self).__init__()
-        self.backend = backend
-        self.frontend = frontend
-
-    def forward(self, x):
-        x = self.backend(x)
-        x = x.view(x.size(0), -1)
-        x = self.frontend(x)
-        return x
+class encoder_decoder:
+    encoder = nn.Sequential(
+        nn.Conv2d(3, 3, (1, 1)),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(3, 64, (3, 3)),
+        nn.ReLU(),  # relu1-1
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(64, 64, (3, 3)),
+        nn.ReLU(),  # relu1-2
+        nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(64, 128, (3, 3)),
+        nn.ReLU(),  # relu2-1
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(128, 128, (3, 3)),
+        nn.ReLU(),  # relu2-2
+        nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(128, 256, (3, 3)),
+        nn.ReLU(),  # relu3-1
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 256, (3, 3)),
+        nn.ReLU(),  # relu3-2
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 256, (3, 3)),
+        nn.ReLU(),  # relu3-3
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 256, (3, 3)),
+        nn.ReLU(),  # relu3-4
+        nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 512, (3, 3)),
+        nn.ReLU(),  # relu4-1, this is the last layer used
+    )
+    decoder = nn.Sequential(
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(512, 256, (3, 3)),
+        nn.ReLU(),
+        nn.Upsample(scale_factor=2, mode='nearest'),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 256, (3, 3)),
+        nn.ReLU(),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 256, (3, 3)),
+        nn.ReLU(),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 256, (3, 3)),
+        nn.ReLU(),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 128, (3, 3)),
+        nn.ReLU(),
+        nn.Upsample(scale_factor=2, mode='nearest'),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(128, 128, (3, 3)),
+        nn.ReLU(),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(128, 64, (3, 3)),
+        nn.ReLU(),
+        nn.Upsample(scale_factor=2, mode='nearest'),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(64, 64, (3, 3)),
+        nn.ReLU(),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(64, 3, (3, 3)),
+    )
