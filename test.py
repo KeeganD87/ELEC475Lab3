80d43128ba8917379c5ed39d847da0fbe68ebc19
Keegan Dillon <keeganegdillon@gmail.com>
2023-11-06 09:38:53 -0500

    created method to calculate top1 and top5

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import argparse
+
 import torch
 import torchvision
 import torchvision.transforms as transforms
@@ -53,5 +55,69 @@ def singleImageTest():
         class_name = class_labels[class_idx]
         print(f"{i+1}. Class: {class_name}, Probability: {class_probabilities:.4f}")
 
+def find_error_rates(args):
+    transform = transforms.Compose([
+        transforms.Resize(size=(256, 256)),
+        transforms.ToTensor()
+    ])
+
+    test_set = torchvision.datasets.CIFAR100(root='../data', train=False, download=True, transform=transform)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=1, shuffle=True)
+
+    encoder = net.encoder_decoder.encoder
+    encoder.load_state_dict(torch.load(args.l))
+
+    model = net.Model(encoder, num_classes=args.c)
+    model.load_state_dict(torch.load(args.s))
+    model.eval()
+
+    if args.cuda.lower() == 'y' and torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+    print("Device: ", device)
+
+    model.to(device)
+    encoder.to(device)
+
+    #Initialize variables for top 1 and top 5 results
+    top1_count = 0
+    top5_count = 0
+
+    num_iterations = len(test_loader)
+    for i, data in enumerate(test_loader, 0):
+        images, labels = data
+
+        with torch.no_grad():
+            images = images.to(device)
+            outputs = model(images)
+
+        _, predicted = torch.topk(outputs, 5, dim=1)
+        predicted = predicted[0].tolist()
+        probabilities = torch.softmax(outputs, dim=1)[0].tolist()
+
+        if predicted[0] == labels[0]:
+            top1_count += 1
+        if labels[0] in predicted:
+            top5_count += 1
+
+        if i % 100 == 0:
+            print(f"Iteration {i}/{num_iterations}")
+
+    top1_percent = (top1_count / len(test_set)) * 100
+    top5_percent = (top5_count / len(test_set)) * 100
+    top1_error = 100 - top1_percent
+    top5_error = 100 - top5_percent
+
+    print(f"Top 1: {top1_percent}% - Error: {top1_error}%")
+    print(f"Top 5: {top5_percent}% - Error: {top5_error}%")
+
 if __name__ == '__main__':
-    singleImageTest()
+    #singleImageTest()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', type=int, default=10, help='Number of classes')
+    parser.add_argument('-l', type=str, default="encoder.pth", help='Encoder path')
+    parser.add_argument('-s', type=str, default="classification_head.pth", help='Model path')
+    parser.add_argument('-cuda', type=str, default='Y', help="Whether to use CPU or Cuda, use Y or N")
+    args = parser.parse_args()
+    find_error_rates(args)
